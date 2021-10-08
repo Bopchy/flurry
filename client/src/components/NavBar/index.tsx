0ed395dedac894f951a9e9b603d578c350032555
Bopchy <rorbochere@gmail.com>
2021-10-09 01:20:10 +0300

    Add search field.

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
-import tw, { styled } from 'twin.macro';
+import React, { useState } from 'react';
+import tw, { styled, css } from 'twin.macro';
 
-const NavBar = styled.nav(() => [tw`border-b`]);
+const Nav = styled.nav(() => [tw`flex items-center px-12`, css`
+  input {
+      background-color: transparent;
+
+      ${[tw`w-3/4 border-0 border-b outline-none`]}
+  }
+`]);
+
+const NavBar = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    setQuery(event.currentTarget.value);
+  }
+ 
+  return (
+    <Nav>
+      <input value={query} onChange={handleChange} placeholder="Search for city..." />
+    </Nav>
+  );  
+};
 
 export default NavBar;

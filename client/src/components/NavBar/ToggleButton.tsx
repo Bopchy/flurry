6f93afe5a6ee75cc144c3c1f6d008df1f31029a7
Bopchy <rorbochere@gmail.com>
2021-12-05 23:58:15 +0300

    Fix object is possibly undefined error. Add ToggleButton interface.

--- client/src/components/NavBar/ToggleButton.tsx
+++ client/src/components/NavBar/ToggleButton.tsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react';
 import { css, styled } from 'twin.macro';
 import { TempFormatContext } from "../../context/tempFormat.context";
+import { IButton } from "../../typings/components";
 
 const Wrapper = styled.span(() => []);
 
 const Button = styled.button(() => [css`border: none; background-color: gray;`]);
 
-const Toggle = () => {
+const Toggle: IButton = () => {
     const temp = useContext(TempFormatContext)
     
     return (
         <Wrapper>
-            <Button onClick={temp.update}>{temp.format}</Button>
+            <Button onClick={temp?.update}>{temp?.format}</Button>
         </Wrapper>
     )
 };
---
+++ client/src/typings/components.tsx
@@ -0,0 +1,3 @@
+export interface IButton {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}

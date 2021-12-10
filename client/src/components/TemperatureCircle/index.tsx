b0d758bedad127ba6ec31ae2197002bf0bacd0b4
Bopchy <rorbochere@gmail.com>
2021-12-10 23:58:21 +0300

    Use tailwind for Circle component.

--- client/src/components/TemperatureCircle/index.tsx
+++ client/src/components/TemperatureCircle/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import tw, { styled, css } from "twin.macro";
 
 import { ITemperatureCircleProps } from "../../utils/types";
 
+const Circle = styled.div(() => [
+    tw`rounded-full border-8 border-white w-72 h-72 flex justify-center`,
+    css`
+      div {
+          $`[tw`border-2 border-black rounded-full w-48 h-48 self-center`]`
+      }
+    `
+]);
+
 const TemperatureCircle: React.FC<ITemperatureCircleProps> = ({ temperature }) => {
     return (
-        <div className="rounded-full border-8 border-white w-72 h-72 flex justify-center">
-            <div className="border-2 border-black rounded-full w-48 h-48 self-center">
+        <Circle>
                 <span className="text-9xl flex p-5">{temperature}</span>
-            </div>
-        </div>
+        </Circle>
     )
 };
 
